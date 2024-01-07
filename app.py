020e3829a7684b5aa94cd1486ed7d1e58e6fe1a7
Tej410 <tejdeep20012013@gmail.com>
2024-01-06 20:39:14 -0500

    added function process_workbook

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import openpyxl as op 
+from openpyxl.chart import BarChart, Reference
 
 wb = op.load_workbook('transactions.xlsx')
 sheet = wb['Sheet1']
@@ -8,4 +9,9 @@ for row in range(2,sheet.max_row+1):
     correct_value = original_value * 0.9
     sheet.cell(row,4).value = correct_value
 
+values = Reference(sheet,min_row=2,max_row=sheet.max_row,min_col=4,max_col=4)
+
+chart = BarChart()
+chart.add_data(values)
+sheet.add_chart(chart,'e2')
 wb.save('transactions2.xlsx')
